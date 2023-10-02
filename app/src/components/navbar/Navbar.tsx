ea9565155764142ff67667c71bfaa32b135cb0dd
lucas <lucas.lvn97439@gmail.com>
2023-10-02 19:57:52 +0400

    make logo clickable to change view

--- app/src/components/navbar/Navbar.tsx
+++ app/src/components/navbar/Navbar.tsx
@@ -1,7 +1,8 @@
+import { Pages, changeView } from "../../App"
 import "./Navbar.css"
 
 export default function (){
     return <nav class="navbar">
-        <p class="logo">Puissance 4 - IA</p>
+        <p class="logo" onClick={() => {changeView(Pages.home)}}>Puissance 4 - IA</p>
     </nav>
 }
