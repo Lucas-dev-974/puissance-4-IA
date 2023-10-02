edfc366fb315ad3db2bd13550a2aef3506c6c9d3
lucas <lucas.lvn97439@gmail.com>
2023-10-02 14:59:35 +0400

    setup eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["solid", "@typescript-eslint/eslint-plugin"],
+  extends: [
+    "plugin:solid/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: [".eslintrc.js", "node_modules", "dist"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    "solid/reactivity": "warn",
+    "solid/no-destructure": "warn",
+    "solid/jsx-no-undef": "error",
+    "@typescript-eslint/no-namespace": "off",
+    quotes: "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
