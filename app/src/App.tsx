e464a90702b6d10d1caf41f8d4deca9f7140f758
lucas <lucas.lvn97439@gmail.com>
2023-10-02 19:42:42 +0400

    use game view

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,29 @@
 import { Match, Switch, createSignal, type Component } from 'solid-js';
 import Navbar from './components/navbar/Navbar';
+import Game from './views/Game';
 import Home from './views/Home';
 
-enum Pages {
+export enum Pages {
   home = "home",
   game = "game"
 }
 
+const [page, setOnPage] = createSignal<Pages>(Pages.home)
+export function changeView(view: Pages){
+  setOnPage(view)
+}
+
 const App: Component = () => {
-  const [page, setOnPage] = createSignal<Pages>(Pages.home)
 
   return <main id='main-app'>
     <Navbar />
     <div class="pt-10">
       <Switch>
         <Match when={page() == Pages.home}>
-          
           <Home />
+        </Match>
+        <Match when={page() == Pages.game}>
+          <Game />
         </Match>
       </Switch>
     </div>
