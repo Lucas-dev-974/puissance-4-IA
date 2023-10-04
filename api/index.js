89a4db731246924c666d901c581dcace0b07b42e
lucas <lucas.lvn97439@gmail.com>
2023-10-04 15:12:48 +0400

    instanciate socker io

--- api/index.js
+++ api/index.js
@@ -3,14 +3,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.APP_PORT;
 app.get('/', (req, res) => {
-    res.send("l'application puissance 4 API est en cour de de construction");
+    res.send("l'application puissance 4 API est en cour de de construction, teste 1");
 });
-app.listen(port, () => {
+const httpServer = new http_1.Server(app);
+httpServer.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+// Websoket
+const io = new socket_io_1.Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+});
+io.on("connection", (socket) => {
+    console.log("connexion effectué");
 });
--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,7 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
+import { Server as HTTPServer } from 'http';
+import { Server } from 'socket.io';
 
 dotenv.config();
 
@@ -10,6 +12,18 @@ app.get('/', (req: Request, res: Response) => {
   res.send("l'application puissance 4 API est en cour de de construction, teste 1");
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+const httpServer = new HTTPServer(app)
+httpServer.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+})
+
+// Websoket
+const io = new Server(httpServer, {
+  cors:{
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("connexion effectué");
 });
